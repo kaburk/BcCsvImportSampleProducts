29db6e355f442d3fbf3e0e759f17668b56d660e8
kaburk <github@kaburk.com>
2026-04-11 07:23:44 +0900

    add menu

--- config.php
+++ config.php
@@ -5,4 +5,10 @@ return [
     'description' => __d('baser_core', 'BcCsvImportCore の活用サンプルプラグインです。商品サンプルテーブルへのCSVインポート機能を提供します。'),
     'author' => 'kaburk',
     'url' => 'https://blog.kaburk.com/',
+    'adminLink' => [
+        'prefix' => 'Admin',
+        'plugin' => 'BcCsvImportSampleProducts',
+        'controller' => 'SampleProductsCsvImports',
+        'action' => 'index',
+    ],
 ];
